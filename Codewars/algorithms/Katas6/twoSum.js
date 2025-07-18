f14374021bf9118dfa82da3b99c3316797924efa
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-07-18 18:39:19 -0300

    chore: add better solution (On) for twoSum problem by using a hashMap

--- Codewars/algorithms/Katas6/twoSum.js
+++ Codewars/algorithms/Katas6/twoSum.js
@@ -9,14 +9,33 @@ Based on: http://oj.leetcode.com/problems/two-sum/
 
 twoSum [1, 2, 3] 4 === (0, 2) */
 
-function twoSum(numbers, target){
-    for (var i = 0; i < numbers.length-1; i++) {
-        for (var j = i+1; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === target) return [i, j];
-        }
-    }
-}
+// function twoSum(numbers, target){
+//     for (var i = 0; i < numbers.length-1; i++) {
+//         for (var j = i+1; j < numbers.length; j++) {
+//             if (numbers[i] + numbers[j] === target) return [i, j];
+//         }
+//     }
+// }
 
 //Prueba
-// console.log( twoSum([1, 2, 3], 4) ); 
+// console.log( twoSum([1, 2, 3], 4) );
 
+function twoSum(numbers, target) {
+  const map = {};
+
+  for (let i = 0; i < numbers.length; i++) {
+    const num = numbers[i];
+    const complement = target - num;
+
+    if (map[complement] !== undefined) {
+      return [map[complement], i];
+    } else {
+      map[num] = i;
+    }
+  }
+
+  return [];
+}
+
+// console.log(twoSum([1, 2, 3], 4));
+console.log(twoSum([2, 7, 5, 11, 13], 7));
